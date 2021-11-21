7087df447df9c5574033fecee03b968acc612faa
swordcube <49110074+swordcube@users.noreply.github.com>
2021-11-21 16:53:14 -0600

    Initial commit

---
+++ source/HealthIcon.hx
@@ -0,0 +1,66 @@
+package;
+
+import flixel.FlxG;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.FlxSprite;
+import openfl.utils.Assets as OpenFlAssets;
+
+using StringTools;
+
+class HealthIcon extends FlxSprite
+{
+	public var sprTracker:FlxSprite;
+	private var isOldIcon:Bool = false;
+	private var isPlayer:Bool = false;
+	private var char:String = '';
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		isOldIcon = (char == 'bf-old');
+		this.isPlayer = isPlayer;
+		changeIcon(char);
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+
+	public function swapOldIcon() {
+		if(isOldIcon = !isOldIcon) changeIcon('bf-old');
+		else changeIcon('bf');
+	}
+
+	public function changeIcon(char:String) {
+		if(this.char != char) {
+			var name:String = 'icons/' + char;
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
+			var file:Dynamic = Paths.image(name);
+
+			var file:FlxAtlasFrames = Paths.getSparrowAtlas('icons/icon-' + char);
+			frames = file;
+			
+			animation.addByPrefix('normal', 'normal', 24, true);
+			animation.addByPrefix('dead', 'dead', 24, true);
+			animation.addByPrefix('win', 'win', 24, true);
+			animation.play('normal');
+			this.char = char;
+
+			antialiasing = ClientPrefs.globalAntialiasing;
+			if(char.endsWith('-pixel')) {
+				antialiasing = false;
+			}
+		}
+	}
+
+	public function getCharacter():String {
+		return char;
+	}
+}
